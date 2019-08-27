f7bff26e4a439717bc51c2a5451cfd89139d0e26
Mónica Sánchez Santos <monni@MacBook-Pro-de-Yoshua-2.local>
2019-08-27 02:11:15 -0500

    Add CORS

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const bodyParser = require("body-parser");
 const db = require("./db/models");
-// db.sequelize.sync({ force: true });
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const SenseiTypesRouter = require("./routes/sensei_types");
@@ -14,6 +14,7 @@ app.set("port", process.env.PORT || 3000);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use("/sensei-types", SenseiTypesRouter);
 app.use("/user", UserRouter);
 app.use("/goal", GoalRouter);
