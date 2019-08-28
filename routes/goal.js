522db48a654077d221ff62db8d23088337213589
Mónica Sánchez Santos <monni@MacBook-Pro-de-Yoshua-2.local>
2019-08-27 23:48:53 -0500

    create goal endpoint

--- routes/goal.js
+++ routes/goal.js
@@ -4,6 +4,39 @@ const jwt = require("jsonwebtoken");
 const Router = express.Router();
 const senei_health = require("../controlers/sensei_health");
 
+Router.post("/", async function(request, response) {
+  const name = request.body.name;
+  const price = request.body.price;
+  const months = request.body.months;
+  if (!name) {
+    return response.status(400).send("Mandatory field");
+  }
+  if (!price) {
+    return response.status(400).send("Goal can't be $0.00");
+  }
+  if (months < 6 || !months) {
+    return response.status(400).send("Goal need to be more than six months");
+  }
+  const token = request.headers.authorization;
+  const privateKey = process.env.SECRET_KEY;
+
+  try {
+    const decoded = jwt.verify(token, privateKey);
+    const newGoal = await db.Goal.create({
+      name: name,
+      price: price,
+      months: months,
+      amountToBe: price / months,
+      id_user: decoded.id
+    });
+    response.json(newGoal);
+  } catch (error) {
+    console.log(error);
+    response.status(400).send("Unexpected error");
+  }
+  response.end();
+});
+
 Router.get("/", async function(request, response) {
   try {
     const token = request.headers.authorization;
@@ -34,7 +67,7 @@ Router.get("/update", async function(request, response) {
     const decoded = jwt.verify(token, privateKey);
 
     const fristSaving = await db.Saving.findOne({
-      where: { id_goal: decoded.id_goal },
+      where: { id_goal: decoded.id },
       order: [["createdAt", "ASC"]]
     });
     //controller
